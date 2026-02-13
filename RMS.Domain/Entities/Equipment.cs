44c9e16d25d12983edb7f2d13c15e85598cb6523
Wryley155 <johnlloydmanilag15@gmail.com>
2026-02-13 08:51:16 +0800

    1

---
+++ RMS.Domain/Entities/Equipment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS.Domain.Entities
+{  
+    public class Equipment
+    {    
+        [Key]
+        public int SerialNumber { get; set; }
+        [Required]  
+        public string Model { get; set; }
+        [Required]  
+        public string Category { get; set; }
+        public LoanStatus status { get; set; }
+    }
+}
